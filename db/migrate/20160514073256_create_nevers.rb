b9e0ba56f14a47fc377395d0ef4ba7eff5a0d149
Rp7f <westhouse51@gmail.com>
2016-05-15 03:09:34 +0900

    change schemafile and change apis db

--- db/migrate/20160514073256_create_nevers.rb
+++ db/migrate/20160514073256_create_nevers.rb
@@ -2,11 +2,16 @@ class CreateNevers < ActiveRecord::Migration
   def change
     create_table :nevers do |t|
       t.string :post
+      t.string :url
+      t.string :src
       t.integer :fav
       t.integer :view
-      t.date :date
+      t.integer :pos_x
+      t.integer :pos_y
+      t.integer :pos_z      
 
       t.timestamps null: false
+      add_index :nevers, :id
     end
   end
 end
